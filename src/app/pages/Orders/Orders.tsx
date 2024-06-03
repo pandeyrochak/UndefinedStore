f9c1450062ed7006066052410698970ab3b3667f
Rochak Pandey <rochakpandey56@gmail.com>
2024-06-03 14:43:10 +0530

    ✨ added orders tracking page. In progress..

--- src/app/pages/Orders/Orders.tsx
+++ src/app/pages/Orders/Orders.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react'
+
 const Orders = () => {
-  return <div className="container-center">Orders</div>
+  const [orders, setOrders] = useState(JSON.parse(localStorage.getItem('userOrders') || '[]'))
+  console.log(orders)
+
+  return (
+    <div className="container-center">
+      <h1>Your Orders</h1>
+      <div className="orders-container">
+        {orders.map(order => {
+          return (
+            <div className="order-card">
+              <h2>Order ID: {order.orderId}</h2>
+
+              {order.orderDetails.map(orderItem => {
+                return (
+                  <div className="order-item flex items-center gap-3">
+                    <h3>{orderItem.name}</h3>
+                    <p>{orderItem.quantity} units</p>
+                    <p>₹{orderItem.price}</p>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Orders
